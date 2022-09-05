e378a01a643018c0fb520647ef1aff727a26eb21
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2022-09-05 10:55:13 -0300

    mv oops

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 mkdir dist || 1
 mkdir dist/server || 1
+mkdir dist/server/node_modules || 1
 cd server || exit
 npm install || exit
 npm run build || exit
 mv dist/* ../dist/server || exit
-mv node_modules ../dist/server || exit
+mv node_modules/* ../dist/server/node_modules || exit
 cd .. || exit
 cp -r client dist || exit
