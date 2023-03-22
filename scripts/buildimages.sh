ff32d9f7a67c553ae5f4afe4bdb3fed04c22d61a
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2023-03-22 19:49:35 -0300

    change scripts

--- scripts/buildimages.sh
+++ scripts/buildimages.sh
@@ -9,3 +9,5 @@ fi
 
 docker build -t qxjourney-client -f "$REPO_DIR/client/docker/Dockerfile" "$REPO_DIR/client"
 docker build -t qxjourney-server "$REPO_DIR/server"
+
+docker rmi "$(docker images --filter dangling=true -q)"
