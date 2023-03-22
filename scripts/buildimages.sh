6610c2c663592ff33339df414531df25b93b801f
DaviXavier <47609623+davirxavier@users.noreply.github.com>
2023-03-22 20:05:34 -0300

    Update buildimages.sh

--- scripts/buildimages.sh
+++ scripts/buildimages.sh
@@ -9,5 +9,3 @@ fi
 
 docker build -t qxjourney-client -f "$REPO_DIR/client/docker/Dockerfile" "$REPO_DIR/client"
 docker build -t qxjourney-server "$REPO_DIR/server"
-
-docker rmi "$(docker images --filter dangling=true -q)"
