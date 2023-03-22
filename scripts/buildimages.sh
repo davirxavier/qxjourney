69f72296ab3800601cc66471cef66e15625094f8
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2023-03-22 19:15:32 -0300

    Add build tools

---
+++ scripts/buildimages.sh
@@ -0,0 +1,11 @@
+REPO_DIR=$1
+REPO_LINK=$2
+
+if [ -d "$REPO_DIR" ]; then
+  (cd "$REPO_DIR" && git pull)
+else
+  git clone "$REPO_LINK" "$REPO_DIR"
+fi
+
+docker build -t qxjourney-client -f "$REPO_DIR/client/docker/Dockerfile" "$REPO_DIR/client"
+docker build -t qxjourney-server "$REPO_DIR/server"
